d02f6d0bad48181f0d23f1c0dde6ae85732cc649
Daniel Banach <djbanach@comcast.net>
2021-11-18 13:45:58 -0700

    fixed onsubmit problems

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React  from 'react';
 import { Component } from 'react';
 import './App.css';
 
-import { Movie } from "./Components/Movie";
+/*import { Movie } from "./Components/Movie"; */
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
   }
 
   updateInputValue(e) {
-    this.setState({
-      value: e.target.value
-    });
+    this.setState({value: e.target.value});
   }
 
   handleSubmit(e) {
+    alert(this.state.value);
     e.preventDefault();
-    console.log(this.state.value);
   }
 
 
@@ -26,10 +27,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div class="title">Movie Descriptions</div>
-          <div class="form-container">
-            <input type="text" value={this.state.value} onChange={e => this.updateInputValue(e)}></input>
-            <button onSubmit={this.handleSubmit}>submit</button>
+          <div>Movie Descriptions</div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.value} onChange={this.updateInputValue}></input>
+              <button>submit</button>
+            </form>
           </div>
         </header>
       </div>
