d684ff7209cf4f7b46b055eba08d10879390ee92
Daniel Banach <djbanach@comcast.net>
2021-11-17 19:05:32 -0700

    created movie component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 class="title">Movie Descriptions</h1>
-      </header>
-    </div>
-  );
+import { Movie } from "./Components/Movie";
+
+const API_KEY = "e5ed686b";
+const url = `http://www.omdbapi.com/?apikey=${API_KEY}&t=`;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 class="title">Movie Descriptions</h1>
+            <Movie />
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Movie.js
@@ -0,0 +1,10 @@
+import {useEffect, useState} from "react";
+
+
+const Movie = (props) => {
+    return (
+        <H1>Hi</H1>
+    )
+}
+
+export default Movie;
